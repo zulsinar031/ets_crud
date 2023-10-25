0813e112eedf872dbf2ee7a546f19e8978341114
zulsinar031 <138203306+zulsinar031@users.noreply.github.com>
2023-10-25 15:37:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+import hashlib 
+import json
 
 app = Flask(__name__)
 
@@ -11,35 +13,41 @@ app.config['MYSQL_DB'] = 'ets_db'
 
 mysql = MySQL(app)
 
+def check_user_exists(cursor, username):
+    cursor.execute("""SELECT * FROM user WHERE user_name = %s""", ([username]))
+    if cursor.fetchone() is None:
+        return False
+    else:
+        return True
+
 #@app.route('/')
 @app.route('/ets_login', methods =['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+    if request.method == "POST":
+        #user_id = uuid4()
+        username = json.loads(request.data)["user_name"]
+        password = json.loads(request.data)["user_password"]
+        password = hashlib.sha256(password.encode("utf-8")).hexdigest()
 
-        if not email or not password:
-            return "Please fill in all fields."
+        # Check if user exists
+        cur = mysql.connection.cursor()
+        #if check_user_exists(cur, username):
+        #    return jsonify({"status": "failed", "message": "User already exists."})
 
-        db = mysql.connection
-        cursor = db.cursor()
-
-        cursor.execute("SELECT user_id, user_name FROM users WHERE user_email = %s AND user_password = %s",
-                       (email, password))
-        user = cursor.fetchone()
-
-        if user:
-            session['loggedin'] = True
-            session['user_id'] = user['user_id']
-            session['user_name'] = user['user_name']
-            session['user_email'] = user['user_email']
-            mesage = 'Logged in successfully !'
-            return render_template('ets_user.html', mesage = mesage)
-        else: 
-            mesage = 'Please enter the correct email/password!'
-        cursor.close()
-        db.close()
-    return render_template('ets_login.html')
+        cur.execute(
+            """SELECT * FROM user(user_name, user_password) VALUES (%s, %s, %s)""",
+            ([username], [password]),
+        )
+        mysql.connection.commit()
+        user = cur.fetchone()
+        cur.close()
+        if user :
+            #session["token"] = username
+            session["token"] = user[0]
+            session["username"] = user[1]
+            return jsonify({"status": "success", "message": "Succesfully log in"})
+        else:
+            return jsonify({"Credentials incorrect"})
 
 @app.route('/logout')
 def logout():
@@ -50,33 +58,32 @@ def logout():
 
 @app.route('/ets_register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        name = request.form['user_name']
-        email = request.form['user_email']
-        password = request.form['user_password']
+    if request.method == "POST":
+        #user_id = uuid4()
+        username = json.loads(request.data)["user_name"]
+        email = json.loads(request.data)["user_email"]
+        password = json.loads(request.data)["user_password"]
+        password = hashlib.sha256(password.encode("utf-8")).hexdigest()
 
-        if not name or not email or not password:
-            return "Please fill in all fields."
+        # Check if user exists
+        cur = mysql.connection.cursor()
+        if check_user_exists(cur, username):
+            return jsonify({"status": "failed", "message": "User already exists."})
 
-        db = mysql.connection
-        cursor = db.cursor()
+        cur.execute(
+            """INSERT INTO users (user_name, user_email, user_password) VALUES (%s, %s, %s)""",
+            ([username], [email], [password]),
+        )
+        mysql.connection.commit()
+        cur.close()
 
-        existing_user = cursor.fetchone()
-        if existing_user:
-            return "User with this email already exists. Please use a different email."
-
-        try:
-            cursor.execute("INSERT INTO users (user_name, user_email, user_password) VALUES (%s, %s, %s)",
-                        (name, email, password))
-            db.commit()
-            cursor.close()
-            db.close()
-            mesage = "Registration sucessful"
-            return render_template('ets_login.html', mesage = mesage)
-        except Exception as e:
-                return render_template('ets_register.html', mesage="registration failed")
-
-    return render_template('ets_register.html')
+        # Success
+        return jsonify({"status": "success", "message": "Registration successful."})
+    else:
+        is_login = False
+        if session.get("token"):
+            is_login = True
+        return render_template("ets_register.html", is_login=is_login)
 
 if __name__ == '__main__':
     app.run(debug=True)
